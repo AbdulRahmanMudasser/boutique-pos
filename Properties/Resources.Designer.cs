c1d44d7abc6f3b441357af367851f42c33935be8
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2025-07-11 17:59:12 +0500

    frontend-backend: major updates

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MaqboolFashionPOS.Properties
+namespace MaqboolFashion.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace MaqboolFashionPOS.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MaqboolFashionPOS.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MaqboolFashion.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
