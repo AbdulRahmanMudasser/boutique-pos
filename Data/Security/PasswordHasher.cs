69b8222eefbfcd6b53794fe6153425b3df1a97c9
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2025-07-12 16:29:18 +0500

    frontend, backend-feat: created signup form, added user service, password helper and added assets

---
+++ Data/Security/PasswordHasher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MaqboolFashion.Data.Security
+{
+    public static class PasswordHasher
+    {
+        public static (string Hash, string Salt) CreateHash(string password)
+        {
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                byte[] salt = new byte[16];
+                rng.GetBytes(salt);
+                string saltStr = Convert.ToBase64String(salt);
+
+                using (var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 10000))
+                {
+                    byte[] hash = pbkdf2.GetBytes(20);
+                    return (Convert.ToBase64String(hash), saltStr);
+                }
+            }
+        }
+
+        public static bool Verify(string password, string storedHash, string storedSalt)
+        {
+            byte[] salt = Convert.FromBase64String(storedSalt);
+            using (var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 10000))
+            {
+                byte[] hash = pbkdf2.GetBytes(20);
+                return Convert.ToBase64String(hash) == storedHash;
+            }
+        }
+    }
+}
